8c70b7db2e50caa4b48eae5c69f01f5e4ec8a5d1
strabucco <sallyberrystorm@gmail.com>
2016-01-27 13:06:47 -0500

    load and process EZH2 ChIP-Chip data. two different results: one with all EZH2 peaks in centroblasts, the other with peaks that ONLY occur in centroblasts (ie not in hESCs)

--- YY1_CHIP.R
+++ YY1_CHIP.R
@@ -5,6 +5,7 @@
 library(ChIPseeker)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(org.Hs.eg.db)
+library(dplyr)
 
 #load YY1 ChIP-Seq data and load peaks
 YY1File<-readPeakFile("wgEncodeSydhTfbsGm12878Yy1StdPk.narrowPeak.bed")
@@ -12,9 +13,16 @@ YY1File<-readPeakFile("wgEncodeSydhTfbsGm12878Yy1StdPk.narrowPeak.bed")
 YY1Anno<-annotatePeak(YY1File, tssRegion = c(-3000, 3000), 
                       TxDb =TxDb.Hsapiens.UCSC.hg19.knownGene, annoDb = "org.Hs.eg.db")
 YY1Names<-as.data.frame(YY1Anno)
-head(YY1Names)
+
 #download and load EZH2 file
+EZH2File<-read.csv("EZH2Table.csv")
 #filter file by EZH2 peaks in CB but not ESCs, also remove those without peaks
+EZH2Only<-EZH2File[-c(1,3,5, 7:11)]
+EZH2CB<-EZH2Only[grepl("CB,EZH2",EZH2Only$CB.EZH2.bound),] #list of all with EZH2 peaks in centroblasts
+EZH2CBOnly<-EZH2CB[!grepl("hESC,EZH2", EZH2CB$hESC.EZH2.bound),] #list of genes with EZH2 peaks ONLY
+#in centroblasts-so this excludes genes that also appear in hESCs
+
+head(EZH2CBOnly)
 #also filter file by EZH2 peaks in CB (all)
 
 
