d6d03a68c3935c7bf6d819820886564b6ba5fadb
strabucco <sallyberrystorm@gmail.com>
2016-01-27 11:02:08 -0500

    Initial Commit with plan for script

---
+++ YY1_CHIP.R
@@ -0,0 +1,16 @@
+#R script for downloading and analyzing ChIP data
+#This will be used to compare ChIP binding between YY1 and EZH2 in B-cells
+
+#load packages needed
+
+#download data for YY1 ChIP-Seq
+#load peaks
+#annotate YY1 file
+
+#download and load EZH2 file
+#filter file by EZH2 peaks in CB but not ESCs, also remove those without peaks
+#also filter file by EZH2 peaks in CB (all)
+
+
+#compare gene names from YY1 annotation to EZH2 filtered files ($ORF)
+#get number of gene names overlapping and a CSV or output list of names
