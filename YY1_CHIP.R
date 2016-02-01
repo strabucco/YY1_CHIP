3ade7e4de79ffb86f5511624656bf4981e620c0a
strabucco <sallyberrystorm@gmail.com>
2016-02-01 13:22:32 -0500

    comparing only peaks in the promoter regions of YY1 rather than ALL YY1 peaks.

--- YY1_CHIP.R
+++ YY1_CHIP.R
@@ -13,18 +13,24 @@ YY1File<-readPeakFile("wgEncodeSydhTfbsGm12878Yy1StdPk.narrowPeak.bed")
 YY1Anno<-annotatePeak(YY1File, tssRegion = c(-3000, 3000), 
                       TxDb =TxDb.Hsapiens.UCSC.hg19.knownGene, annoDb = "org.Hs.eg.db")
 YY1Names<-as.data.frame(YY1Anno)
+#filter YY1 list by only promoters for comparison because EZH2 list is based on ChIP-CHIP,
+#which only used probes in the promoter region, so we really are only able to compare what ChIPs
+#to promoter regions anyway.
+YY1Promoter<-YY1Names[grepl("Promoter", YY1Names$annotation),]
 
 #download and load EZH2 file
 EZH2File<-read.csv("EZH2Table.csv")
 #filter file by EZH2 peaks in CB but not ESCs, also remove those without peaks
 EZH2Only<-EZH2File[-c(1,3,5, 7:11)]
+#also filter file by EZH2 peaks in CB (all)
 EZH2CB<-EZH2Only[grepl("CB,EZH2",EZH2Only$CB.EZH2.bound),] #list of all with EZH2 peaks in centroblasts
 EZH2CBOnly<-EZH2CB[!grepl("hESC,EZH2", EZH2CB$hESC.EZH2.bound),] #list of genes with EZH2 peaks ONLY
 #in centroblasts-so this excludes genes that also appear in hESCs
 
-head(EZH2CBOnly)
-#also filter file by EZH2 peaks in CB (all)
-
-
 #compare gene names from YY1 annotation to EZH2 filtered files ($ORF)
+bothCBOnly<-intersect(EZH2CBOnly$ORF, YY1Promoter$SYMBOL)
+str(bothCBOnly)
+bothAll<-intersect(EZH2CB$ORF,YY1Promoter$SYMBOL)
+str(bothAll)
+head(bothAll)
 #get number of gene names overlapping and a CSV or output list of names
