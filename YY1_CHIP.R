8dab6283ae21294de4207a91241c7134a29f047a
strabucco <sallyberrystorm@gmail.com>
2016-02-03 09:51:55 -0500

    Remove duplicate gene names and NAs making each list used to compare a list of only unique gene names.

--- YY1_CHIP.R
+++ YY1_CHIP.R
@@ -17,6 +17,9 @@ YY1Names<-as.data.frame(YY1Anno)
 #which only used probes in the promoter region, so we really are only able to compare what ChIPs
 #to promoter regions anyway.
 YY1Promoter<-YY1Names[grepl("Promoter", YY1Names$annotation),]
+YY1PromoterClean<-YY1Promoter[!duplicated(YY1Promoter$SYMBOL),]
+YY1noNA<-YY1PromoterClean[!(is.na(YY1PromoterClean$SYMBOL)), ]
+
 
 #download and load EZH2 file
 EZH2File<-read.csv("EZH2Table.csv")
@@ -26,11 +29,15 @@ EZH2Only<-EZH2File[-c(1,3,5, 7:11)]
 EZH2CB<-EZH2Only[grepl("CB,EZH2",EZH2Only$CB.EZH2.bound),] #list of all with EZH2 peaks in centroblasts
 EZH2CBOnly<-EZH2CB[!grepl("hESC,EZH2", EZH2CB$hESC.EZH2.bound),] #list of genes with EZH2 peaks ONLY
 #in centroblasts-so this excludes genes that also appear in hESCs
+EZH2CBClean<-EZH2CB[!duplicated(EZH2CB$ORF),]
+EZH2CBnoNA<-EZH2CBClean[!(is.na(EZH2CBClean$ORF)),]
+EZH2CBOnlyClean<-EZH2CBOnly[!duplicated(EZH2CBOnly$ORF),]
+EZH2CBOnlynoNA<-EZH2CBOnlyClean[!(is.na(EZH2CBOnlyClean$ORF)),]
 
 #compare gene names from YY1 annotation to EZH2 filtered files ($ORF)
-bothCBOnly<-intersect(EZH2CBOnly$ORF, YY1Promoter$SYMBOL)
+bothCBOnly<-intersect(EZH2CBOnlynoNA$ORF, YY1noNA$SYMBOL)
 str(bothCBOnly)
-bothAll<-intersect(EZH2CB$ORF,YY1Promoter$SYMBOL)
+bothAll<-intersect(EZH2CBnoNA$ORF,YY1noNA$SYMBOL)
 str(bothAll)
 head(bothAll)
 #get number of gene names overlapping and a CSV or output list of names
